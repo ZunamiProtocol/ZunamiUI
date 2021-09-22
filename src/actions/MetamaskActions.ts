1f25966ba21f832de3523dbcfd3fbf0aea781426
Alexey Senichev <senichevalexey@gmail.com>
2021-09-22 18:44:01 +0300

    Added functionality for getting wallet address

--- src/actions/MetamaskActions.ts
+++ src/actions/MetamaskActions.ts
@@ -7,11 +7,21 @@ export const isWalletConnected = (): boolean => {
 export const connectWallet = async () => {
     if (!isWalletConnected()) {
         alert('Connect your wallet');
+
+        try {
+            await ethereum.request({method: 'eth_requestAccounts'});
+        } catch (error) {
+            console.log(error);
+        }
+    }
+};
+
+export const getWalletAddrs = async (): Promise<string[] | null> => {
+    if (!isWalletConnected()) {
+        alert('Connect your wallet');
+        return null;
     }
 
-    try {
-        await ethereum.request({method: 'eth_requestAccounts'});
-    } catch (error) {
-        console.log(error);
-    }
+    const accounts: string[] = await ethereum.request({method: 'eth_accounts'});
+    return accounts;
 };
