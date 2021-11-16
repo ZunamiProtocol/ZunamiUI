b2a99f39bd9e9b11dde7316499a68b6ea56e22b6
Ruslan Nigmatulin <avadon.90@gmail.com>
2021-11-16 23:23:29 +0300

    Wallet connect fix

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import {connectWallet} from '../../actions/MetamaskActions';
+import { WalletStatus } from '../WalletStatus/WalletStatus';
 import './Header.scss';
 
 export const Header = (): JSX.Element => {
     return (
         <div className={'header'}>
             <img className={'logo'} src='logo.svg' alt='Logo of the Zunami Protocol' />
-            <div onClick={connectWallet} className={'wallet'}>
-                <img src='wallet.svg' alt='' />
-                <p>Connect Wallet</p>
-            </div>
+            <WalletStatus />
         </div>
     );
 };
---
+++ src/components/WalletStatus/WalletStatus.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { connectWallet, getSelectedWalletAddress } from '../../actions/MetamaskActions';
+import './WalletStatus.scss';
+
+export const WalletStatus = (): JSX.Element => {
+    const [address, setAddress] = useState(getSelectedWalletAddress());
+
+    if (address) {
+        const shortAddress = 
+            `${address.substring(0, 6)}...${address.substring(address.length - 4)}`;
+
+        return (
+            <div className={'WalletStatus WalletStatus__connected'}>
+                <img src='wallet-connected.svg' alt='' />
+                <span>{shortAddress}</span>
+            </div>
+        );
+    }
+
+    return (
+        <div
+            className={'WalletStatus'}
+            onClick={async () => {
+                setAddress(await connectWallet());
+            }}
+        >
+            <img src='wallet.svg' alt='' />
+            <span>Connect Wallet</span>
+        </div>
+    );
+};
