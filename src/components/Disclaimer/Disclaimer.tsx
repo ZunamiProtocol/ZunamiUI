44396c61c2fdf96a66966e22221c732e1e38c533
Ruslan Nigmatulin <avadon.90@gmail.com>
2021-11-25 10:41:08 +0300

    Layout + Disclaimer mobile versions

--- src/components/Disclaimer/Disclaimer.tsx
+++ src/components/Disclaimer/Disclaimer.tsx
@@ -7,7 +7,7 @@ interface DisclaimerProps {
 
 export const Disclaimer = (props: DisclaimerProps): JSX.Element => {
     return (
-        <div className={'mb-0 Disclaimer'}>
+        <div className={'Disclaimer'}>
             <img src='/disclaimer.svg' alt='' className={'Disclaimer__icon'} />
             <span className={''}>{props.text}</span>
         </div>
