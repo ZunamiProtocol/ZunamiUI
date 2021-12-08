3237199dede9ac2eaf6b0789df8026dbfefc161f
Ruslan Nigmatulin <404177+Avadon@users.noreply.github.com>
2021-12-08 13:39:31 +0300

    Added chart width to screen width dependency

--- src/components/Chart/Chart.tsx
+++ src/components/Chart/Chart.tsx
@@ -1,5 +1,7 @@
+import { useState, useEffect } from 'react';
 import { PieChart } from 'react-minimal-pie-chart';
 import './Chart.scss';
+import { is1024, is1440, is1920 } from '../../functions/screen';
 
 interface DataItem {
     color: string;
@@ -12,8 +14,8 @@ interface ChartProps {
 }
 
 function renderStratList(items: Array<DataItem>) {
-    return items.map(item => 
-        <div className={'PieChart__StratList__Item'}>
+    return items.map((item, index) => 
+        <div key={index} className={'PieChart__StratList__Item'}>
             <div
                 className={'PieChart__StratList__Item__Circle'}
                 style={{ background: item.color }}
@@ -23,7 +25,33 @@ function renderStratList(items: Array<DataItem>) {
     )
 }
 
+function screenWidthToChartWidth() {
+    let width = 34;
+
+    if (is1024()) {
+        width = 34;
+    }
+
+    if (is1440()) {
+        width = 33;
+    }
+
+    if (is1920()) {
+        width = 39;
+    }
+
+    return width;
+}
+
 export const Chart = (props: ChartProps): JSX.Element => {
+    const [width, setWidth] = useState(screenWidthToChartWidth());
+
+    useEffect(() => {
+        window.addEventListener('resize', (e) => {
+            setWidth(screenWidthToChartWidth());
+        });
+    }, []);
+
     return (
         <div className={'PieChart'}>
             <div className={'PieChart__Header'}>Current strategies</div>
@@ -31,7 +59,7 @@ export const Chart = (props: ChartProps): JSX.Element => {
                 <PieChart
                     data={props.data}
                     totalValue={100}
-                    lineWidth={30}
+                    lineWidth={width}
                     paddingAngle={5}
                     labelPosition={0}
                     className={'PieChart__Chart'}
