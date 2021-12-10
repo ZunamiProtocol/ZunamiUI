603fdd448f040c10ce3395847b478bf3ebaa6837
goblin007 <ltrend@yandex.ru>
2021-12-10 21:36:58 +0700

    delete unused code

--- src/sushi/utils.js
+++ src/sushi/utils.js
@@ -67,67 +67,6 @@ export const getFarms = (sushi) => {
         : [];
 };
 
-export const getPoolWeight = async (masterChefContract, pid) => {
-    const {allocPoint} = await masterChefContract.methods.poolInfo(pid).call();
-    const totalAllocPoint = await masterChefContract.methods
-        .totalAllocPoint()
-        .call();
-    return new BigNumber(allocPoint).div(new BigNumber(totalAllocPoint));
-};
-
-export const getEarned = async (masterChefContract, pid, account) => {
-    return masterChefContract.methods.pendingSushi(pid, account).call();
-};
-
-// export const getTotalLPWethValue = async (
-//   masterChefContract,
-//   wethContract,
-//   lpContract,
-//   tokenContract,
-//   pid,
-// ) => {
-// Get balance of the token address
-// const tokenAmountWholeLP = await tokenContract.methods
-//   .balanceOf(lpContract.options.address)
-//   .call()
-// const tokenDecimals = await tokenContract.methods.decimals().call()
-// // Get the share of lpContract that masterChefContract owns
-// const balance = await lpContract.methods
-//   .balanceOf(masterChefContract.options.address)
-//   .call()
-//
-// const tokenBalanceIVF = new BigNumber(balance).div(
-//   new BigNumber(10).pow(tokenDecimals),
-// )
-//
-// // Convert that into the portion of total lpContract = p1
-// const totalSupply = await lpContract.methods.totalSupply().call()
-// // Get total weth value for the lpContract = w1
-// const lpContractWeth = await wethContract.methods
-//   .balanceOf(lpContract.options.address)
-//   .call()
-// // Return p1 * w1 * 2
-// const portionLp = new BigNumber(balance).div(new BigNumber(totalSupply))
-// const lpWethWorth = new BigNumber(lpContractWeth)
-// const totalLpWethValue = portionLp.times(lpWethWorth).times(new BigNumber(2))
-//
-// const tokenAmount = new BigNumber(tokenAmountWholeLP)
-//   .times(portionLp)
-//   .div(new BigNumber(10).pow(tokenDecimals))
-//
-// const wethAmount = new BigNumber(lpContractWeth)
-//   .times(portionLp)
-//   .div(new BigNumber(10).pow(18))
-//
-// return {
-// tokenAmount,
-// wethAmount,
-// totalWethValue: totalLpWethValue.div(new BigNumber(10).pow(18)),
-// tokenPriceInWeth: wethAmount.div(tokenAmount),
-// poolWeight: await getPoolWeight(masterChefContract, pid),
-// tokenBalanceIVF,
-// }
-// }
 
 export const approve = async (provider, tokenAddress, masterChefContract, account) => {
     const lpContract = getContract(provider, tokenAddress);
