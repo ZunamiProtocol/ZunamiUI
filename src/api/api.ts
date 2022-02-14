405ea9fce7f038ec6109304abc5385d403af6aae
Ruslan Nigmatulin <avadon.90@gmail.com>
2022-02-14 11:21:36 +0300

    Historical APY chart

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,6 +2,7 @@ const root = "https://zunami-reward-api.herokuapp.com/api";
 export const poolStatsUrl = `${root}/pool/stats`;
 export const zunamiInfoUrl = `${root}/zunami/info`;
 export const testnetUrl = `${root}/feature`;
+export const historicalApyUrl = `${root}/zunami/apy-chart`;
 
 export const getPoolStatsUrl = (poolTypes: string): string => {
     return poolStatsUrl + "?types=" + poolTypes;
@@ -9,4 +10,8 @@ export const getPoolStatsUrl = (poolTypes: string): string => {
 
 export const getTestnetStatusUrl = (address: string): string => {
     return `${testnetUrl}?address=${address}`;
+};
+
+export const getHistoricalApyUrl = (period : string): string => {
+    return `${historicalApyUrl}?period=${period.toUpperCase()}`;
 };
