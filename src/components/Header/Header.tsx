1ff37508fbaca296928539a5c47e8b60d382e38c
Ruslan Nigmatulin <avadon.90@gmail.com>
2022-02-17 12:45:39 +0300

    Commented lines removed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,12 +7,10 @@ export const Header = (): JSX.Element => {
 
     return (
         <Navbar expand="lg" className={'header'}>
-            {/* <Container className={'header-container'}> */}
                 <Navbar.Brand href="https://zunami.io">
                     <img className={'Logo'} src={logoVariant} alt="Logo of the Zunami Protocol"/>
                 </Navbar.Brand>
                 <Disclaimer text={<div>Please note. The contract has been <a target="_blank" rel="noreferrer" href="http://ya.ru">audited</a>, but it steel a beta version. Use it at your own risk</div>} />
-            {/* </Container> */}
         </Navbar>
     );
 };
