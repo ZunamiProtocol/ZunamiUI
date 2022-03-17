a7d73162b3d24c9a4490321712a4886442780411
Ruslan Nigmatulin <avadon.90@gmail.com>
2022-03-17 15:35:01 +0300

    usePendingOperation hook

--- src/hooks/usePendingOperations.ts
+++ src/hooks/usePendingOperations.ts
@@ -1,58 +1,87 @@
 import { useState, useEffect } from 'react';
 import { ethers } from 'ethers';
 import { useWallet } from 'use-wallet';
+import BigNumber from 'bignumber.js';
+import { getBalanceNumber } from '../utils/formatbalance';
+import { contractAddresses } from '../sushi/lib/constants';
 
 export interface PendingOperations {
-    deposit: number;
-    withdraw: number;
+    deposit: BigNumber;
+    withdraw: BigNumber;
 }
 
 const usePendingOperations = (): PendingOperations => {
-    const [pendingDeposit, setPendingDeposit] = useState(0);
-    const [pendingWithdraw, setPendingWithdraw] = useState(0);
-    const zunamiAddr = '0xf790d57443C2E9aDEc8C42bA92Bff5B7Ec84A895';
-    const abi = [
-        'event CreatedPendingDeposit(address indexed depositor, uint256[3] amounts)',
-        'event CreatedPendingWithdrawal(address indexed withdrawer, uint256[3] amounts, uint256 lpShares)',
-        'function pendingDeposits(address) public view returns(uint256[3] deposit)',
-        'function pendingWithdrawals(address) public view returns(tuple(uint256 lpShares, uint256[3] withdraw))',
-    ];
+    const [pendingDeposit, setPendingDeposit] = useState(new BigNumber(0));
+    const [pendingWithdraw, setPendingWithdraw] = useState(new BigNumber(0));
+    const zunamiAddr = contractAddresses.zunami[1];
     const { account } = useWallet();
-    const httpProvider = new ethers.providers.Web3Provider(window.ethereum);
-    const contract = new ethers.Contract(zunamiAddr, abi, httpProvider.getSigner());
 
-    const depositEventHandler = (addr: number, amounts: number[]) => {
-        setPendingDeposit(amounts.reduce((prev: number, curr: number): number => prev + curr));
-    };
+    // const depositEventHandler = (addr: number, amounts: number[]) => {
+    //     setPendingDeposit(amounts.reduce((prev: number, curr: number): number => prev + curr));
+    // };
 
-    const withdrawEventHandler = (addr: number, amounts: number[], lpShares: number) => {
-        setPendingWithdraw(amounts.reduce((prev: number, curr: number): number => prev + curr));
-    };
+    // const withdrawEventHandler = (addr: number, amounts: number[], lpShares: number) => {
+    //     setPendingWithdraw(amounts.reduce((prev: number, curr: number): number => prev + curr));
+    // };
 
     useEffect(() => {
-        const initPendings = async () => {
-            const deposit = await contract.pendingDeposits(account);
-            setPendingDeposit(deposit.reduce((prev: number, curr: number): number => prev + curr));
+        if (!window.ethereum) {
+            const test = async () => {
+                setPendingDeposit(new BigNumber(0));
+                setPendingWithdraw(new BigNumber(0));
+            };
 
-            const withdraw = await contract.pendingWithdrawals(account)[1];
-            setPendingWithdraw(
-                withdraw.reduce((prev: number, curr: number): number => prev + curr)
-            );
+            test();
+            return;
+        }
+
+        const abi = [
+            'event CreatedPendingDeposit(address indexed depositor, uint256[3] amounts)',
+            'event CreatedPendingWithdrawal(address indexed withdrawer, uint256[3] amounts, uint256 lpShares)',
+            'function pendingDeposits(address) public view returns(uint256[3])',
+            'function pendingWithdrawals(address) public view returns(uint256)',
+        ];
+        const httpProvider = new ethers.providers.Web3Provider(window.ethereum);
+        const contract = new ethers.Contract(zunamiAddr, abi, httpProvider.getSigner());
+
+        const initPendings = async () => {
+            try {
+                const pendingCoins = await contract.pendingDeposits(account);
+
+                const result = [
+                    new BigNumber(pendingCoins[0].toString()),
+                    new BigNumber(pendingCoins[1].toString()),
+                    new BigNumber(pendingCoins[2].toString()),
+                ];
+
+                setPendingDeposit(result[0].plus(result[1]).plus(result[2]));
+            } catch (error) {
+                debugger;
+            }
+
+            try {
+                const val = await contract.pendingWithdrawals(account);
+                setPendingWithdraw(new BigNumber(val.toString()));
+            } catch (error) {
+                debugger;
+            }
         };
 
-        initPendings();
+        if (contract && account) {
+            initPendings();
+        }
 
-        const filterDeposit = contract.filters.CreatedPendingDeposit(account);
-        const filterWithdraw = contract.filters.CreatedPendingWithdrawal(account);
+        // const filterDeposit = contract.filters.CreatedPendingDeposit(account);
+        // const filterWithdraw = contract.filters.CreatedPendingWithdrawal(account);
 
-        contract.on(filterDeposit, depositEventHandler);
-        contract.on(filterWithdraw, withdrawEventHandler);
+        // contract.on(filterDeposit, depositEventHandler);
+        // contract.on(filterWithdraw, withdrawEventHandler);
 
         return () => {
-            contract.off(filterDeposit, depositEventHandler);
-            contract.off(filterWithdraw, withdrawEventHandler);
+            // contract.off(filterDeposit, depositEventHandler);
+            // contract.off(filterWithdraw, withdrawEventHandler);
         };
-    }, []);
+    }, [account, zunamiAddr]);
 
     const operations: PendingOperations = {
         deposit: pendingDeposit,
