52d7f6c3ab0fc8a1ec06a58fe411ae9e3d150c0a
Ruslan Nigmatulin <404177+Avadon@users.noreply.github.com>
2022-05-29 23:26:26 +0300

    Sidebar default APY/TVL/etc blocks

--- src/__tests__/unit/components/Sidebar.js
+++ src/__tests__/unit/components/Sidebar.js
@@ -27,5 +27,20 @@ describe('<Sidebar>', () => {
         expect(component.children[0].children[0].innerHTML).toBe(
             'Yield Aggregator for Best Stablecoin Staking'
         );
+
+        const sidebar = component.children[0].children[2];
+
+        const tvlBlock = sidebar.children[0];
+        expect(tvlBlock.children[0].textContent).toBe('Total Value Locked');
+        expect(tvlBlock.children[1].outerHTML).toBe('<div class="preloader mt-3"></div>');
+
+        const apyBlock = sidebar.children[1];
+        expect(apyBlock.children[0].children[0].textContent).toBe('Base APY');
+        expect(apyBlock.children[0].children[1].children[0].tagName).toBe('IMG'); // hint icon
+        expect(apyBlock.children[1].outerHTML).toBe('<div class="preloader mt-3"></div>');
+
+        const rewardBlock = sidebar.children[2];
+        expect(rewardBlock.children[0].textContent).toBe('Reward APY');
+        expect(rewardBlock.children[1].textContent).toBe('Soon');
     });
 });
