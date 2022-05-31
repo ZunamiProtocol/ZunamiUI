aa80f42a3c4b17514e9a63264c5803341f41bdfd
Ruslan Nigmatulin <404177+Avadon@users.noreply.github.com>
2022-05-31 10:34:48 +0300

    Default balance value set to -1 in order to distinguish zero balance from loading state

--- src/hooks/useUserLpAmount.ts
+++ src/hooks/useUserLpAmount.ts
@@ -7,7 +7,7 @@ import { getUserLpAmount } from '../utils/erc20';
 import { BIG_ZERO } from '../utils/formatbalance';
 
 const useUserLpAmount = () => {
-    const [allowance, setAllowance] = useState(BIG_ZERO);
+    const [allowance, setAllowance] = useState(new BigNumber(-1));
     const { account } = useWallet();
     const sushi = useSushi();
     const masterChefContract = getMasterChefContract(sushi);
