d5acb032406924c5b4c1cd73308a18bd26049dbd
Ruslan Nigmatulin <avadon.90@gmail.com>
2022-06-13 17:56:21 +0400

    Some components changed to lazy load + ethers, also an icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Main } from './containers/Main';
-import { FinanceOperations } from './containers/FinanceOperations';
-import { Staking } from './containers/Staking';
-import { Lockdrop } from './containers/Lockdrop';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { UseWalletProvider } from 'use-wallet';
 import config from './config';
@@ -14,26 +11,31 @@ import EthereumExplorerProvider from './contexts/EthereumExplorerProvider';
 import SushiProvider from './contexts/SushiProvider';
 import TransactionProvider from './contexts/Transactions';
 import ModalsProvider from './contexts/Modals';
+import {Preloader} from './components/Preloader/Preloader';
+
+const FinanceOperations = React.lazy(
+    () => import('./containers/FinanceOperations').then(module => ({ default: module.FinanceOperations }))
+);
 
 const { INFURA_URL, CHAIN_ID } = config;
 
 function App() {
     return (
-        <Providers>
-            <Router>
-                <Route exact path="/" component={Main} />
-                <Route
-                    path="/deposit"
-                    component={() => <FinanceOperations operationName="Deposit" />}
-                />
-                <Route
-                    path="/withdraw"
-                    component={() => <FinanceOperations operationName="Withdraw" />}
-                />
-                <Route path="/staking" component={() => <Staking />} />
-                <Route path="/lockdrop" component={() => <Lockdrop />} />
-            </Router>
-        </Providers>
+        <Suspense fallback={<Preloader onlyIcon={true} />}>
+            <Providers>
+                <Router>
+                    <Route exact path="/" component={Main} />
+                    <Route
+                        path="/deposit"
+                        component={() => <FinanceOperations operationName="Deposit" />}
+                    />
+                    <Route
+                        path="/withdraw"
+                        component={() => <FinanceOperations operationName="Withdraw" />}
+                    />
+                </Router>
+            </Providers>
+        </Suspense>
     );
 }
 
