eca555ce4d4d0be887878c0da2e8b7bad1acf161
Ruslan Nigmatulin <avadon.90@gmail.com>
2022-07-25 23:56:41 +0300

    BSC gateway update

--- src/sushi/lib/constants.js
+++ src/sushi/lib/constants.js
@@ -5,7 +5,8 @@ export const contractAddresses = {
         // ETH
         1: '0x2ffCC661011beC72e1A9524E12060983E74D14ce',
         // BNB
-        56: '0x02a228D826Cbb1C0E8765A6DB6E7AB64EAA80BFD',
+        // 56: '0x02a228D826Cbb1C0E8765A6DB6E7AB64EAA80BFD',
+        56: '0x299E41CA24896a8A361e118fb04531056e7737A5',
     },
     weth: {
         1: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
