d6771fcd4053320063a24f1cbe029ed654f4993b
Ruslan Nigmatulin <avadon.90@gmail.com>
2022-07-29 13:53:32 +0300

    BSC old balance retrieval (#253)
    * BSC old balance retrieval
    
    * Old BSC gateway balance check no matter what current network is
    
    * Migration button only visible for BSC network
    
    * Deploy to test
    
    * Non-BSC network detection for modal
    
    * Deploy to test

--- src/hooks/useOldBscBalance.ts
+++ src/hooks/useOldBscBalance.ts
@@ -6,7 +6,7 @@ import useSushi from './useSushi';
 import { getMasterChefContract } from '../sushi/utils';
 import Web3 from 'web3';
 import bscAbi from '../actions/abi/zunami_bsc.json';
-import { isBSC, isETH } from '../utils/zunami';
+import { isETH } from '../utils/zunami';
 
 const useOldBscBalance = () => {
     const [balance, setBalance] = useState(new BigNumber(BIG_ZERO));
@@ -16,7 +16,7 @@ const useOldBscBalance = () => {
     const masterChefContract = getMasterChefContract(sushi);
 
     useEffect(() => {
-        if (!account || !chainId || !isBSC(chainId) || !masterChefContract) {
+        if (!account || !chainId || !masterChefContract) {
             setBalance(BIG_ZERO);
             return;
         }
@@ -28,7 +28,7 @@ const useOldBscBalance = () => {
             contract.options.address = '0x02a228D826Cbb1C0E8765A6DB6E7AB64EAA80BFD';
 
             const value = await contract.methods.balanceOf(account).call();
-            console.log(`OLD BSC balance execution (${chainId}). Result: ${value}`);
+            console.log(`OLD BSC balance execution (chain ${chainId}). Result: ${value}`);
 
             if (value) {
                 setBalance(new BigNumber(value));
