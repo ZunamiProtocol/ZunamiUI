9d118bb46f7e85c9c096c9053153aa26fdf137e8
Ruslan Nigmatulin <avadon.90@gmail.com>
2022-08-04 10:06:41 +0300

    Fix

--- src/components/BscMigrationModal/BscMigrationModal.tsx
+++ src/components/BscMigrationModal/BscMigrationModal.tsx
@@ -31,7 +31,7 @@ export const BscMigrationModal = (props: BscMigrationModalProps): JSX.Element =>
     const [isGZLPapproved, setGZLPapproved] = useState(false);
 
     useEffect(() => {
-        if (!zunamiContract || !account || chainId !== 56) {
+        if (!account || chainId !== 56 || !props.show) {
             return;
         }
 
