57cf2e2cfe3a85ebd602f7ecc9d87076205f0b49
Ruslan Nigmatulin <404177+Avadon@users.noreply.github.com>
2022-08-16 10:13:07 +0300

    Added partial modal block

--- src/components/UnsupportedChain/UnsupportedChain.tsx
+++ src/components/UnsupportedChain/UnsupportedChain.tsx
@@ -3,7 +3,7 @@ import './UnsupportedChain.scss';
 export const UnsupportedChain = (): JSX.Element => {
     return (
         <div className="UnsupportedChain">
-            <div>
+            <div className="UnsupportedChain__Content">
                 You're using unsupported chain. Please, switch either to Ethereum or Binance
                 network.
             </div>
