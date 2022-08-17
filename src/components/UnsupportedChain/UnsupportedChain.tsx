dab5ce16035e31dba54ed08e662acf3276d516d1
Ruslan Nigmatulin <404177+Avadon@users.noreply.github.com>
2022-08-17 11:56:13 +0300

    Added network selector in modal

--- src/components/UnsupportedChain/UnsupportedChain.tsx
+++ src/components/UnsupportedChain/UnsupportedChain.tsx
@@ -1,11 +1,30 @@
 import './UnsupportedChain.scss';
 
 export const UnsupportedChain = (): JSX.Element => {
+    const eth = window.ethereum;
+
     return (
         <div className="UnsupportedChain">
             <div className="UnsupportedChain__Content">
-                You're using unsupported chain. Please, switch either to Ethereum or Binance
-                network.
+                <div>
+                    You're using unsupported chain. Please, switch either to Ethereum or Binance
+                    network.
+                </div>
+                <div className="mt-3">
+                    {eth && eth.request && (
+                        <button
+                            onClick={async () => {
+                                await eth.request({
+                                    method: 'wallet_switchEthereumChain',
+                                    params: [{ chainId: '0x1' }],
+                                });
+                            }}
+                            className="zun-button"
+                        >
+                            Change network
+                        </button>
+                    )}
+                </div>
             </div>
         </div>
     );
