01915758fe72653c38e1689338f33ee2c7f2d671
Ruslan Nigmatulin <404177+Avadon@users.noreply.github.com>
2022-08-18 11:12:47 +0300

    Deploy to test: gas limit decreased

--- src/sushi/utils.js
+++ src/sushi/utils.js
@@ -16,6 +16,8 @@ BigNumber.config({
     DECIMAL_PLACES: 80,
 });
 
+export const GAS_LIMIT = '500000';
+
 export const getMasterChefAddress = (sushi) => {
     return sushi && sushi.masterChefAddress;
 };
@@ -108,7 +110,7 @@ export const approve = async (
         .approve(masterChefContract.options.address, sum)
         .send({
             from: account,
-            gas: '1000000',
+            gas: GAS_LIMIT,
         })
         .on('transactionHash', (tx) => {
             return tx.transactionHash;
@@ -147,7 +149,7 @@ export const stake = async (contract, account, dai, usdc, usdt, direct = false, 
         log(`Zunami contract: execution deposit(${coins})`);
         return contract.methods
             .deposit(coins)
-            .send({ from: account, gas: '1000000' })
+            .send({ from: account, gas: GAS_LIMIT })
             .on('transactionHash', (tx) => {
                 return tx.transactionHash;
             });
@@ -157,7 +159,7 @@ export const stake = async (contract, account, dai, usdc, usdt, direct = false, 
 
     return contract.methods
         .delegateDeposit(coins)
-        .send({ from: account, gas: '1000000' })
+        .send({ from: account, gas: GAS_LIMIT })
         .on('transactionHash', (tx) => {
             return tx.transactionHash;
         });
@@ -209,7 +211,7 @@ export const unstake = async (
 
         return zunamiContract.methods
             .delegateWithdrawal(lpShares, coins)
-            .send({ from: account, gas: '1000000' })
+            .send({ from: account, gas: GAS_LIMIT })
             .on('transactionHash', (transactionHash) => {
                 return transactionHash;
             });
@@ -217,7 +219,7 @@ export const unstake = async (
         log(`Zunami contract: execution withdraw(${lpShares}, [0, 0, 0], 1, ${coinIndex})`);
         return zunamiContract.methods
             .withdraw(lpShares, [0, 0, 0], 1, coinIndex)
-            .send({ from: account, gas: '1000000' })
+            .send({ from: account, gas: GAS_LIMIT })
             .on('transactionHash', (transactionHash) => {
                 return transactionHash;
             });
