89b08cddf9190c3585e8de44c68e625b82d2f59b
Ruslan Nigmatulin <404177+Avadon@users.noreply.github.com>
2022-08-19 14:02:31 +0300

    Deploy to test: Improved network change modal

--- src/components/UnsupportedChain/UnsupportedChain.tsx
+++ src/components/UnsupportedChain/UnsupportedChain.tsx
@@ -1,7 +1,10 @@
+import { useState } from 'react';
+import { NetworkSelector, networks, Network } from '../NetworkSelector/NetworkSelector';
 import './UnsupportedChain.scss';
 
 export const UnsupportedChain = (): JSX.Element => {
     const eth = window.ethereum;
+    const [activeNetwork, setActiveNetwork] = useState<Network>(networks[0]);
 
     return (
         <div className="UnsupportedChain">
@@ -10,18 +13,26 @@ export const UnsupportedChain = (): JSX.Element => {
                     You're using unsupported chain. Please, switch either to Ethereum or Binance
                     network.
                 </div>
-                <div className="mt-3">
+                <div className="mt-3 text-center">
+                    <NetworkSelector
+                        className="ms-0"
+                        defaultNetwork={activeNetwork}
+                        autoChange={false}
+                        onNetworkChange={(network: Network) => {
+                            setActiveNetwork(network);
+                        }}
+                    />
                     {eth && eth.request && (
                         <button
                             onClick={async () => {
                                 await eth.request({
                                     method: 'wallet_switchEthereumChain',
-                                    params: [{ chainId: '0x1' }],
+                                    params: [{ chainId: activeNetwork.key }],
                                 });
                             }}
-                            className="zun-button"
+                            className="zun-button mt-3"
                         >
-                            Change network
+                            Switch
                         </button>
                     )}
                 </div>
