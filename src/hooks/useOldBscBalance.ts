717627a8ffbccc2515d135c001aa2ab72b12dd32
Ruslan Nigmatulin <avadon.90@gmail.com>
2022-09-12 18:53:50 +0300

    BSC gateway migration to 1.2 (#288)
    * BSC gateway migration to 1.2
    
    * USDT approve fix for BSC
    
    * Extra margin for approve buttons
    
    * Better logs, withdraw validation error, approve detection fix
    
    * calcWithdrawOneCoin logs
    
    * Hide Idea files
    
    Co-authored-by: Mikhail Zelenin <mikhail.zelenin@gmail.com>

--- src/hooks/useOldBscBalance.ts
+++ src/hooks/useOldBscBalance.ts
@@ -6,11 +6,27 @@ import useSushi from './useSushi';
 import { getMasterChefContract } from '../sushi/utils';
 import Web3 from 'web3';
 import bscAbi from '../actions/abi/zunami_bsc.json';
-import { isBSC, isETH } from '../utils/zunami';
+import { isETH } from '../utils/zunami';
 import { log } from '../utils/logger';
+import { contractAddresses } from '../sushi/lib/constants';
+
+async function getDeprecatedBalance(address: string, account: string) : Promise<string> {
+    const web3 = new Web3(new Web3.providers.HttpProvider('https://bscrpc.com'));
+    const contract = new web3.eth.Contract(bscAbi);
+    contract.options.from = account;
+    contract.options.address = address;
+
+    const value = await contract.methods.balanceOf(account).call();
+    log(`Deprecated BSC contract (${address}) balance execution. Result: ${value}`);
+    return value;
+}
 
 const useOldBscBalance = () => {
-    const [balance, setBalance] = useState(new BigNumber(BIG_ZERO));
+    const [balance, setBalance] = useState([
+        new BigNumber(BIG_ZERO), // 1.0
+        new BigNumber(BIG_ZERO), // 1.1
+    ]);
+
     const { chainId, account } = useWallet();
     const isEth = isETH(chainId);
     const sushi = useSushi();
@@ -18,22 +34,18 @@ const useOldBscBalance = () => {
 
     useEffect(() => {
         if (!account || !chainId || !masterChefContract) {
-            setBalance(BIG_ZERO);
+            setBalance([
+                BIG_ZERO,
+                BIG_ZERO
+            ]);
             return;
         }
 
         const getBalance = async () => {
-            const web3 = new Web3(new Web3.providers.HttpProvider('https://bscrpc.com'));
-            const contract = new web3.eth.Contract(bscAbi);
-            contract.options.from = account;
-            contract.options.address = '0x02a228D826Cbb1C0E8765A6DB6E7AB64EAA80BFD';
-
-            const value = await contract.methods.balanceOf(account).call();
-            log(`OLD BSC balance execution (${chainId}). Result: ${value}`);
-
-            if (value) {
-                setBalance(new BigNumber(value));
-            }
+            setBalance([
+                new BigNumber(await getDeprecatedBalance(contractAddresses.deprecated.v_1_0_bsc, account)),
+                new BigNumber(await getDeprecatedBalance(contractAddresses.deprecated.v_1_1_bsc, account)),
+            ]);
         };
 
         getBalance();
--- src/sushi/lib/constants.js
+++ src/sushi/lib/constants.js
@@ -5,7 +5,10 @@ export const contractAddresses = {
         // ETH
         1: '0x2ffCC661011beC72e1A9524E12060983E74D14ce',
         // BNB
-        56: '0xFEdcBA60B3842e3F9Ed8BC56De171da5426AF8CF',
+        56: '0xeAC5e2b6F1d7eBF4a715a235e097b59ACa40b786',
+    },
+    zunami_bsc_migrator: {
+        v1_1_to_1_2: '0xBCabe08134b6895d7Da8b7a0C0241cfbD29A4bc2',
     },
     weth: {
         1: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
@@ -16,4 +19,8 @@ export const contractAddresses = {
     busd: {
         56: '0x4a062f1501f5FF149b973b70f7027d87622445F3',
     },
+    deprecated: {
+        v_1_0_bsc: '0x02a228D826Cbb1C0E8765A6DB6E7AB64EAA80BFD',
+        v_1_1_bsc: '0xFEdcBA60B3842e3F9Ed8BC56De171da5426AF8CF',
+    }
 };
