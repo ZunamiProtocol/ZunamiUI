e734838211c0a619215ad34dee50686677589ae8
Ruslan Nigmatulin <avadon.90@gmail.com>
2023-02-20 13:40:04 +0300

    LP price changed to the strategy's

---
+++ src/hooks/useStratLpPrice.ts
@@ -0,0 +1,38 @@
+import BigNumber from 'bignumber.js';
+import { useEffect, useState } from 'react';
+import { BIG_ZERO } from '../utils/formatbalance';
+import { useWallet } from 'use-wallet';
+import useSushi from './useSushi';
+import { getMasterChefContract } from '../sushi/utils';
+
+const useStratLpPrice = () => {
+    const { chainId, account } = useWallet();
+    const sushi = useSushi();
+    const masterChefContract = getMasterChefContract(sushi, chainId);
+    const [price, setPrice] = useState(new BigNumber(BIG_ZERO));
+
+    useEffect(() => {
+        if (!account || !chainId || !masterChefContract || !sushi) {
+            return;
+        }
+
+        const getLpPrice = async () => {
+            const contract = sushi.getEthContract();
+            const totalHoldings = await contract.methods.totalHoldings().call();
+            const wPid = await contract.methods.defaultWithdrawPid().call();
+            const pool = await contract.methods.poolInfo(wPid).call();
+
+            const lpPrice = new BigNumber(totalHoldings).dividedBy(new BigNumber(pool.lpShares));
+            setPrice(lpPrice);
+        };
+
+        getLpPrice();
+
+        let refreshInterval = setInterval(getLpPrice, 10000);
+        return () => clearInterval(refreshInterval);
+    }, [account, chainId, masterChefContract, sushi]);
+
+    return price;
+};
+
+export default useStratLpPrice;
